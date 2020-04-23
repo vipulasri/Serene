c8a4f5e86f81962cda2a4cc6e13efb10791c4067
vipulasri <vipulasri.2007@gmail.com>
2020-04-24 04:01:48 +0530

    update: audios to support ios

--- lib/manager/audio_manager.dart
+++ lib/manager/audio_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:serene/model/sound.dart';
@@ -17,11 +19,17 @@ class AudioManager {
 
   AudioManager._internal() {
     playing = Map();
+    //AudioPlayer.logEnabled = true;
   }
 
   play(Sound sound) async {
     if (!playing.containsKey(sound.id)) {
       AudioCache player = AudioCache();
+      if (Platform.isIOS) {
+        if (player.fixedPlayer != null) {
+          player.fixedPlayer.startHeadlessService();
+        }
+      }
       playing[sound.id] = await player.loop(sound.audio, volume: sound.volume);
     }
     playing[sound.id].setVolume(sound.volume);
