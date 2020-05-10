dc8c39b13b861a78d6da4266436dea9e574d2910
vipulasri <vipulasri.2007@gmail.com>
2020-05-10 20:43:23 +0530

    remove: un-needed resources

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,13 +1,6 @@
-import 'package:flutter/cupertino.dart';
-
 class Assets {
   static const baseImagesPath = 'assets/images/';
-  static const homeBackground = baseImagesPath + 'bg_home.jpg';
   static const soundsJson = 'assets/sounds.json';
   static const baseSoundsPath = 'audios/';
   static const logoAnimation = 'assets/animation/serene-logo.flr';
-
-  void preCacheAssets(BuildContext context) {
-    precacheImage(AssetImage(homeBackground), context);
-  }
 }
